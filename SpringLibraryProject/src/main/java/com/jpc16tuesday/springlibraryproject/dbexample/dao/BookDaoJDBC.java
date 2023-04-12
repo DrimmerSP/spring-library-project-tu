7ef7f5d8fc54501e2fb06622c136476977e9f49d
Denis Romashov <denisromashov95@gmail.com>
2023-04-12 20:07:48 +0300

    added jdbc dao

---
+++ SpringLibraryProject/src/main/java/com/jpc16tuesday/springlibraryproject/dbexample/dao/BookDaoJDBC.java
@@ -0,0 +1,42 @@
+package com.jpc16tuesday.springlibraryproject.dbexample.dao;
+
+import com.jpc16tuesday.springlibraryproject.dbexample.model.Book;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookDaoJDBC {
+    public Book findBookById(Integer bookId) {
+        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/local_db",
+                "postgres", "12345")) {
+
+            if (connection != null) {
+                System.out.println("Ура! Мы подключились к БД!!!");
+            } else {
+                System.out.println("БД недоступна...");
+            }
+            String select = "select * from books where id = ?";
+            PreparedStatement selectQuery = connection.prepareStatement(select);
+            selectQuery.setInt(1, bookId);
+            ResultSet resultSet = selectQuery.executeQuery();
+
+            List<Book> books = new ArrayList<>();
+            while (resultSet.next()) {
+                Book book = new Book();
+                book.setAuthor(resultSet.getString("author"));
+                book.setTitle(resultSet.getString("title"));
+                book.setDateAdded(resultSet.getDate("date_added"));
+//                books.add(book);
+                System.out.println(book);
+                return book;
+            }
+
+
+
+        } catch (SQLException ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+        return null;
+    }
+}
